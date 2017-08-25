1fc57e56dd86cc72dfb9777eab16ca6d87370e91
Lucio Delelis <ldelelis@est.frba.utn.edu.ar>
2017-08-24 21:19:50 -0300

    Modified some index stuff

--- server.js
+++ server.js
@@ -5,5 +5,6 @@ var serveStatic = require('serve-static');
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/static'));
 
 app.listen(8080);
